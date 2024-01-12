662a9b884367afe924eca0613b8d0bc3a342e8ba
Ritvik Gupta <108052380+Ritvik2205@users.noreply.github.com>
2024-01-12 21:45:20 +0000

    navbar-js-add

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // loading the DOM Content
+
 $(function(){
     $("#nav-placeholder").load("nav.html");
+
+    
 });
 
-$(".navbar").on( "click", "a", function() {
-    $(this).addClass('active').siblings().removeClass('active');
-    });
+
