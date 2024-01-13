c8656fe48d97fc327c0853137b3975b28c0f20e0
Ritvik Gupta <108052380+Ritvik2205@users.noreply.github.com>
2024-01-13 01:27:44 +0000

    theme-switcher-added

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ $(function(){
     $("#nav-placeholder").load("nav.html");
 });
 
+
+$(".home-img-clickable").on("click", function(e) {
+    
+    var currentTheme = $("html").attr("data-theme");
+    if (currentTheme === "light") {
+        $("html").attr("data-theme", "dark");
+    } else {
+        $("html").attr("data-theme", "light");
+    }
+    e.preventDefault();
+});   
