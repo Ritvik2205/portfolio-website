af4b9694d0c1724a618a700b90a9275a1c8bd9a6
Ritvik Gupta <108052380+Ritvik2205@users.noreply.github.com>
2024-03-12 21:10:47 +0000

    12-03-2024 commit

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
-// loading the DOM Content
-$(function(){
-    $("#nav-placeholder").load("nav.html");
+// loading the DOM Content   
+$(document).ready(function(e) {
+    $(function(){
+        $("#nav-placeholder").load("nav.html");
+    });
+
+    var currentTheme = $("html").attr("data-theme");
+    console.log(localStorage.getItem('theme'));
+    if(localStorage.getItem('theme')){
+        currentTheme = localStorage.getItem('theme');
+        $("html").attr("data-theme", currentTheme);
+    }
+    
+    
+    $(".home-image img").on("click", function(e) {
+        
+        $(document).load("index.html");
+        var currentTheme = $("html").attr("data-theme");
+        console.log(localStorage.getItem('theme'));
+        if(localStorage.getItem('theme')){
+            currentTheme = localStorage.getItem('theme');
+            $("html").attr("data-theme", currentTheme);
+        }
+        if (currentTheme === "light") {
+            $("html").attr("data-theme", "dark");
+            localStorage.setItem('theme', 'dark');
+        } else {
+            $("html").attr("data-theme", "light");
+            localStorage.setItem('theme', 'light');
+        }
+        e.preventDefault();
+    }); 
+
+    var typed = new Typed(".text-effect", {
+        strings: ["CS Student", "Artist", "Author"],
+        typeSpeed: 100,
+        backSpeed: 75,
+        backDelay: 1000,
+        loop: true
+    })
 });
 
 
-$(".home-img-clickable").on("click", function(e) {
-    
-    var currentTheme = $("html").attr("data-theme");
-    if (currentTheme === "light") {
-        $("html").attr("data-theme", "dark");
-    } else {
-        $("html").attr("data-theme", "light");
-    }
-    e.preventDefault();
-});   
+// // adding event listener to navbar burger
+// $(".navbar-burger").click(function() {
+//     $(this).toggleClass("is-active");
+//     var target = $(".navbar-menu");
+//     if (target.hasClass('is-hidden')){
+//         target.removeClass('is-hidden');
+//         } else {
+//             target.addClass('is-hidden')
+//             };
+// })
